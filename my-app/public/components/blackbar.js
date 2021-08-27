9830d121d43398be7e32c0d7a0755881a3f198c4
Diamond Rosales <roserosales288@gmail.com>
2021-08-27 00:42:10 -0400

    work

---
+++ my-app/public/components/blackbar.js
@@ -0,0 +1,32 @@
+function Blackbar(){
+    return (
+        <>
+   
+            <div className="top-bar d-none d-md-block">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="top-bar-left">
+                            <div className="text">
+                               
+                            </div>
+                            <div className="text">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="top-bar-right">
+                            <div className="social">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+        </>
+    )
+}
+
+export default Blackbar
